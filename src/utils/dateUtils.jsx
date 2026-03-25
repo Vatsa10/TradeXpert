ab1770ca5d47d04f4d498e7c346a4deed44dc8c1
Vatsa10 <vatsajoshi2@gmail.com>
2026-03-25 21:25:06 +0530

    cleanup the codebase and add the new code

--- src/utils/dateUtils.jsx
+++
@@ -1,7 +0,0 @@
-export const formatUTCDate = (date) => {
-    return date.toISOString().slice(0, 19).replace('T', ' ');
-  };
-  
-  export const getCurrentUTCDateTime = () => {
-    return formatUTCDate(new Date());
-  };
